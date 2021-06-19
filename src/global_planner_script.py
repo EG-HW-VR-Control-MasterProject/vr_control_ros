8f3336f429c8f661ea000ecb6fab7735527a0edf
EnzoGhisoni <enzo.ghisoni@hotmail.fr>
2021-06-19 21:17:29 +0200

    Add a script to test and publish new message list

--- src/global_planner_script.py
+++ src/global_planner_script.py
@@ -2,31 +2,57 @@
 # -*- coding: utf-8 -*-
 import rospy
 from nav_msgs.msg import Path, OccupancyGrid, Odometry
-from std_msgs.msg import String, Int16
+from std_msgs.msg import String, Int32
 from geometry_msgs.msg import PoseStamped
+#from itemPose.msg import itemPose
+from master_project.msg import itemPoseList, itemPose
 from math import *
 
 class PathPlanner():
 	def __init__(self):
 		rospy.init_node("path_planner", anonymous=True)
-
+		self.choice_index_sub = rospy.Subscriber('/item/choice_item_index', Int32, self.choice_index_cb)
+		#self.index_list_sub = rospy.Subscriber('/item/item_list', itemPose, self.index_list_cb)
+		self.index_list_sub = rospy.Subscriber('/item/item_list', itemPoseList, self.index_list_cb)
 		self.goal_pub = rospy.Publisher('/global_planner/goal', PoseStamped, queue_size=10)
 		self.planner_costmap_pub = rospy.Publisher('/global_planner/costmap/costmap', OccupancyGrid, queue_size=10)
 
 		self.choice_index = 0
 		self.choice_index_prev = 0
 
+		self.item_selected = itemPose()
+
+		self.updatedItemList = itemPoseList()
+		#self.updatedItemList = []
+
 		self.goalMsg = PoseStamped()
 		self.goalMsg.header.frame_id = "map"
-		self.goalMsg.pose.position.x = 2
-		self.goalMsg.pose.position.y = -3
+		self.goalMsg.pose.position.x = 0
+		self.goalMsg.pose.position.y = 0
 		self.goalMsg.pose.orientation.z = 0.0
 		self.goalMsg.pose.orientation.w = 1.0
 		self.goalMsg.header.stamp = rospy.Time.now()
 
 		while not rospy.is_shutdown():
-
-			self.goal_pub.publish(self.goalMsg)
+			if len(self.updatedItemList.item_pose_list) != 0:
+				print("Update objective")
+				self.item_selected = self.updatedItemList.item_pose_list[self.choice_index]
+				print("Selected Item " + str(self.item_selected))
+				#self.item_selected = self.updatedItemList[self.choice_index]
+				self.goalMsg.pose = self.item_selected.pose
+				#print("1er " + str(self.updatedItemList.item_pose_list[0]))
+				#print("2nd " + str(self.updatedItemList.item_pose_list[1]))
+				#print("The goal is x:" + str(self.goalMsg.pose.position.x)+" y:" + str(self.goalMsg.pose.position.y))
+				self.goal_pub.publish(self.goalMsg)
+	
+	def choice_index_cb(self, msg):
+		self.choice_index = msg.data
+		#if self.choice_index_prev != self.choice_index:
+	
+	def index_list_cb(self, msg):
+		self.updatedItemList = msg
+		#self.updatedItemList.append(msg)
+	
 
 def main():
 	PathPlanner()
---
+++ src/publish_list_test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import rospy
+from nav_msgs.msg import Path, OccupancyGrid, Odometry
+from std_msgs.msg import String, Int16
+from geometry_msgs.msg import PoseStamped
+#from itemPose.msg import itemPose
+from master_project.msg import itemPoseList, itemPose
+from math import *
+
+class GoalSender():
+	def __init__(self):
+		rospy.init_node("goal_sender", anonymous=True)
+		self.choice_index_pub = rospy.Publisher('/item/choice_item_index', Int16, queue_size=10)
+		self.index_list_pub = rospy.Publisher('/item/item_list', itemPoseList, queue_size=10)
+
+		self.choice_index = 0
+		self.choice_index_prev = 0
+
+		self.counter = 0
+
+		self.item_selected = itemPose()
+		self.item_selected_second = itemPose()
+		self.item_selected_third = itemPose()
+
+		self.updatedItemList = itemPoseList()
+		
+		self.item_selected.id = "1st"
+		self.item_selected.pose.position.x = -2
+		self.item_selected.pose.position.y = -3
+
+		self.updatedItemList.item_pose_list.append(self.item_selected)
+
+		self.item_selected_second.id = "2nd"
+		self.item_selected_second.pose.position.x = 4
+		self.item_selected_second.pose.position.y = -2
+		self.updatedItemList.item_pose_list.append(self.item_selected_second)
+
+		self.item_selected_third.id = "3rd"
+		self.item_selected_third.pose.position.x = 2
+		self.item_selected_third.pose.position.y = -1
+		self.updatedItemList.item_pose_list.append(self.item_selected_third)
+
+
+		while not rospy.is_shutdown():
+			#if self.counter < 3:
+			print(self.updatedItemList)
+			self.index_list_pub.publish(self.updatedItemList)
+			self.counter += 1
+	
+	
+
+def main():
+	GoalSender()
+	try:
+		rospy.spin()
+	except rospy.KeyboardInterrupt:
+		print("Shutting down")
+
+if __name__ == '__main__':
+	main()
